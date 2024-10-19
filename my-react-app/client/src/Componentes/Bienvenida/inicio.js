39a7adf029832f6265a71cb26473f776d036acb2
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-18 23:21:58 -0300

    a

--- my-react-app/client/src/Componentes/Bienvenida/inicio.js
+++ my-react-app/client/src/Componentes/Bienvenida/inicio.js
@@ -8,7 +8,7 @@ function Inicio() {
             <div class="bloqueprincipal">
             <div style={{ marginLeft: '5%' }}>
                 <div class="w3-container">
-                <h1>¡BIENVENIDO!</h1>
+                <h1>¡Bienvenido!</h1>
                 <h3>Bienvenido a la aplicación de registro web de Outlet A Tu Hogar.</h3>
                 <p>En el menú de su izquierda podrá encontrar las acciones disponibles para el manejo del negocio.
                 </p>
