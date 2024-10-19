09bfcaac8eca1621d2642f8da2602acb26c40e5e
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-19 03:35:53 -0300

    YAPO

--- my-react-app/server/Routes/productList.js
+++ my-react-app/server/Routes/productList.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getProducts } = require('../Controllers/productListController'); // Asegúrate de que esta línea sea correcta
 
 // Define la ruta para obtener los productos
-router.get('/', getProducts); // Esta ruta llamará al controlador getProducts
+router.get('/products', getProducts); // Esta ruta llamará al controlador getProducts
 
 module.exports = router;
