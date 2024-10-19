2da1601b88a2e5a0d07d21153f1e7e8896a26a89
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-19 18:04:19 -0300

    reporte

--- my-react-app/client/src/Componentes/Reportes/ReporteGral.js
+++ my-react-app/client/src/Componentes/Reportes/ReporteGral.js
@@ -26,13 +26,14 @@ function ReporteGral() {
     setMenosVendidos(dataMenos);
   };
 
-  const handleBuscar = () => {
+  const handleBuscar = (e) => {
+    e.preventDefault(); // Prevenir la recarga de la página
     obtenerReportes();
   };
 
   useEffect(() => {
     document.title = 'Reporte';
-}, []);
+  }, []);
 
   return (
     <div style={{ marginLeft: '12%' }}>
@@ -45,7 +46,7 @@ function ReporteGral() {
             </legend>
             <div className="account-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
               <div style={{ flex: '1' }}>
-              <h2>{ventasMensuales !== null ? ventasMensuales : 'Cargando...'}</h2>
+                <h2>{ventasMensuales !== null ? ventasMensuales : 'Cargando...'}</h2>
               </div>
               <div style={{ flex: '1' }}>
                 <label>Desde</label>
@@ -63,44 +64,45 @@ function ReporteGral() {
                   onChange={(e) => setFechaFin(e.target.value)} 
                 />
               </div>
-            
-            <div style={{ flex: '1' , alignSelf: 'center'}} >
-            <button onClick={handleBuscar} style={{ width:'50px', padding: '5px 10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}><i className="fa fa-search"></i></button>
-            </div>
+              <div style={{ flex: '1' , alignSelf: 'center'}} >
+                <button 
+                  type="submit" // Tipo submit si lo manejas en un formulario
+                  style={{ width:'50px', padding: '5px 10px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
             </div>
           </fieldset>
         </form>
 
-      
-        {/*<h2>Ventas Mensuales: {ventasMensuales !== null ? ventasMensuales : 'Cargando...'}</h2>*/}
         <fieldset>
-        <legend>
-        <h3>Top Productos</h3>
-        </legend>
-        <ul>
-          {topProductos.map((producto, index) => (
-            <li key={index}>
-              <strong>Producto:</strong> {producto[0]} <br />
-              <strong>Cantidad Vendida:</strong> {producto[1]} <br />
-              <strong>Precio:</strong> ${producto[2]}
-            </li>
-          ))}
-        </ul>
+          <legend>
+            <h3>Top Productos</h3>
+          </legend>
+          <ul>
+            {topProductos.map((producto, index) => (
+              <li key={index}>
+                <strong>Producto:</strong> {producto[0]} <br />
+                <strong>Cantidad Vendida:</strong> {producto[1]} <br />
+                <strong>Precio:</strong> ${producto[2]}
+              </li>
+            ))}
+          </ul>
         </fieldset> 
 
-         <fieldset> 
-         <legend>
-        <h3>Productos Menos Vendidos</h3>
-        </legend>
-        <ul>
-          {menosVendidos.map((producto, index) => (
-            <li key={index}>
-              <strong>Producto:</strong> {producto[0]} <br />
-              <strong>Cantidad Vendida:</strong> {producto[1]} <br />
-              <strong>Precio:</strong> ${producto[2]}
-            </li>
-          ))}
-        </ul>
+        <fieldset> 
+          <legend>
+            <h3>Productos Menos Vendidos</h3>
+          </legend>
+          <ul>
+            {menosVendidos.map((producto, index) => (
+              <li key={index}>
+                <strong>Producto:</strong> {producto[0]} <br />
+                <strong>Cantidad Vendida:</strong> {producto[1]} <br />
+                <strong>Precio:</strong> ${producto[2]}
+              </li>
+            ))}
+          </ul>
         </fieldset> 
       </div>
     </div>
