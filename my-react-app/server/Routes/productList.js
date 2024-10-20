e2c1682b4ebd8557c26aba2e2f20961bbd2bfcd4
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-20 05:39:26 -0300

    Y NO SE COMO

--- my-react-app/server/Routes/productList.js
+++ my-react-app/server/Routes/productList.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { getProducts } = require('../Controllers/productListController'); // Asegúrate de que esta línea sea correcta
 
 // Define la ruta para obtener los productos
-router.post('/', getProducts); // Esta ruta llamará al controlador getProducts
+router.get('/', getProducts); // Esta ruta llamará al controlador getProducts
 
 module.exports = router;
