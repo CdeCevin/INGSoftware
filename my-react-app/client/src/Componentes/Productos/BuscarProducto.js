94211afcf1b9eab3cb4f24c47e7d37677e5d8555
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-21 11:03:27 -0300

    alt añadido

--- my-react-app/client/src/Componentes/Productos/BuscarProducto.js
+++ my-react-app/client/src/Componentes/Productos/BuscarProducto.js
@@ -100,9 +100,10 @@ const BuscarProducto = () => {
                 {/* Modal para mostrar mensajes */}
                 <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Mensaje">
                     <h2>Mensaje</h2>
-                    <img>src={"C:/Users/Koliv/Desktop/todo/Nueva carpeta/Outlet/310.jpg"}</img>
+                    <img src={"C:/Users/Koliv/Desktop/todo/Nueva carpeta/Outlet/310.jpg"} alt="Imagen del producto" />
                     <button onClick={closeModal}>Cerrar</button>
                 </Modal>
+
 
                 {productos.length > 0 ? (
                     <table className="venta-table">
