4cf9cfdce6cd14de1bd02e2098e3eb58dc01f9f6
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-21 16:39:35 -0300

    aaa

--- my-react-app/client/src/Componentes/Productos/BuscarProducto.js
+++ my-react-app/client/src/Componentes/Productos/BuscarProducto.js
@@ -45,9 +45,9 @@ const BuscarProducto = () => {
         }
     };
 
-    // Función para manejar la visualización de la imagen
+    // Función para manejar la visualización de la imagen images/Outlet/
     const mostrarImagen = (codigo_producto) => {
-        const imageUrl = `/images/Outlet/${codigo_producto}.jpg`; // Usamos una ruta relativa
+        const imageUrl = `C:/Users/Koliv/Desktop/todo/Nueva carpeta/Outlet/${codigo_producto}.jpg`; // Usamos una ruta relativa
         setSelectedImage(imageUrl); // Establecer la URL de la imagen seleccionada
         setModalIsOpen(true); // Abrir el modal con la imagen
     };
