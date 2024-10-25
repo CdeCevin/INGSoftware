74f04c2f0a4a9b0933fe5c4d6a1c5f895ebfc749
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-25 04:15:31 -0300

    a

--- my-react-app/client/src/Componentes/Ventas/VentaClienteEx.js
+++ my-react-app/client/src/Componentes/Ventas/VentaClienteEx.js
@@ -1,11 +1,86 @@
 import React from 'react';
 
 function VentaClienteEx() {
+    const [codigo, setCodigo] = useState('');
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Preparar los datos en formato JSON
+        const formData = {
+            inputNombre: nombre || null,
+            inputCod: codigo,
+            inputStock: stock || null,
+            inputPrecio: precio || null,
+            inputStockmin: stockmin || null,
+        };
+        console.log('Datos del formulario:', formData);
+
+        try {
+            // Enviar los datos al backend
+            const response = await fetch('http://localhost:3001/api/up_producto/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setModalMessage(data.message); // Mostrar mensaje de éxito
+                resetForm();
+            } else {
+                const errorData = await response.json();
+                setModalMessage(errorData.message); // Mostrar mensaje de error
+            }
+        } catch (error) {
+            console.error('Error al enviar el formulario:', error);
+            setModalMessage('Error al enviar el formulario.'); // Mensaje de error genérico
+        } finally {
+            setModalIsOpen(true); // Abrir el modal después de intentar enviar el formulario
+        }
+    };
+
     return (
-        <div>
+        <div style={{ marginLeft: '12%' }}>
+            <div className="main-block">
+                <form onSubmit={handleSubmit}>
+                    <h1>Actualizar Producto</h1>
+                    <fieldset>
+                        <legend>
+                            <h3>Producto a Editar</h3>
+                        </legend>
+                        <div className="account-details" style={{ display: 'flex', flexDirection: 'column' }}>
+                            <div>
+                                <label>Código cliente*</label>
+                                <input 
+                                    type="text" 
+                                    name="input-cod" 
+                                    pattern="[0-9]+" 
+                                    maxLength="4" 
+                                    required 
+                                    value={codigo} 
+                                    onChange={(e) => setCodigo(e.target.value)} 
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+                    <button type="submit">Buscar</button>
+                </form>
+            </div>
+            {/* Modal para mostrar mensajes */}
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Mensaje">
+                <h2>Mensaje</h2>
+                <p>{modalMessage}</p>
+                <button onClick={closeModal}>Cerrar</button>
+            </Modal>
         </div>
-            
     );
 }
+
+
+
 
 export default VentaClienteEx;
