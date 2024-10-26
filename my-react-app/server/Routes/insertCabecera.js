cb88f1125eaddad60e167485b206546b9f7ccb02
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-26 17:58:28 -0300

    a

--- my-react-app/server/Routes/insertCabecera.js
+++ my-react-app/server/Routes/insertCabecera.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { insertarCabecera } = require('../Controllers/insertCabeceraController');
 
-// Ruta para obtener las ventas pendientes
-router.get('/', insertarCabecera);
+router.post('/', insertarCabecera);
 
 
 
