ac5567bdeda8e8ff972f6e694eb0d13eb2e3916f
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-10-28 04:09:01 -0300

    lupa

--- my-react-app/client/src/Componentes/Ventas/MostrarBoleta.js
+++ my-react-app/client/src/Componentes/Ventas/MostrarBoleta.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
-function MostrarBoleta() {
+function Boleta() {
     const [boleta, setBoleta] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:3000/api/boleta', {
-            method: 'GET'
-        })
+        fetch('http://localhost:3000/api/boleta')
             .then(response => response.json())
-            .then(data => {
-                console.log("Datos recibidos:", data); // Verifica la estructura aquí
-                setBoleta(data);
-            })
+            .then(data => setBoleta(data))
             .catch(error => console.error('Error al obtener la boleta:', error));
     }, []);
-    
+
     if (!boleta) return <div>Cargando boleta...</div>;
 
     return (
@@ -58,4 +53,4 @@ function MostrarBoleta() {
     );
 }
 
-export default MostrarBoleta;
+export default Boleta;
