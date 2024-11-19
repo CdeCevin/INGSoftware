acbe457e968c9a02ea260c277439728a123e214f
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2024-11-19 10:36:23 -0300

    A

--- my-react-app/server/Controllers/stockCriticoController.js
+++ my-react-app/server/Controllers/stockCriticoController.js
@@ -16,7 +16,7 @@ const obtenerProductosBajoStock = async (req, res) => {
 
         const resultSet = result.outBinds.cursor_resultado;
         console.log(resultSet.Codigo_Producto);
-        console.log(cursor_resultado.Codigo_Producto);
+        
         const rows = [];
         let row;
 
