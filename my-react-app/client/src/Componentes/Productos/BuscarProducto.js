9c8bba23360d5b7d41a9218733818fc09ab065d4
Domi <92610173+dt-24@users.noreply.github.com>
2024-11-21 12:51:08 -0300

    ,,,

--- my-react-app/client/src/Componentes/Productos/BuscarProducto.js
+++ my-react-app/client/src/Componentes/Productos/BuscarProducto.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import '../../Estilos/style_menu.css';
 import '../../Estilos/estilo.css';
 import Modal from 'react-modal';
