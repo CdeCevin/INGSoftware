5a9e998e6aeda269b906e3b7f506bcc782ef0147
CdeCevin <kolivaresfuentes@gmail.com>
2025-05-17 15:43:26 -0400

    a

--- my-react-app/client/src/App.js
+++ my-react-app/client/src/App.js
@@ -1,72 +1,70 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Navigate, Route, Routes } from 'react-router-dom';
 import Login from './Componentes/login/login';
-import Menu from './Componentes/Bienvenida/menu';
-import Home from './Componentes/Bienvenida/Home';
-import Inicio from './Componentes/Bienvenida/inicio';
-// ... (resto de importaciones)
+import Menu from '../src/Componentes/Bienvenida/menu';
+import Home from '../src/Componentes/Bienvenida/Home';
+import Inicio from '../src/Componentes/Bienvenida/inicio';
 
-function LayoutWithMenu({ children }) {
-  const { pathname } = useLocation();
-  const hideMenuOn = ['/', '/login']; // rutas donde no queremos el menu
-  const shouldHideMenu = hideMenuOn.includes(pathname);
+//Clientes
+import ActualizarCliente from '../src/Componentes/Clientes/ActualizarCliente';
+import BuscarCliente from '../src/Componentes/Clientes/BuscarCliente';
+import EliminarCliente from '../src/Componentes/Clientes/EliminarCliente';
+import ListadoClientes from '../src/Componentes/Clientes/ListadoClientes';
+//Empresa
+import ActualizarDatos from '../src/Componentes/Empresa/ActualizarDatos';
+import VisualizarDatos from '../src/Componentes/Empresa/VisualizarDatos';
+//Pendientes
+import VentasPendientes from '../src/Componentes/Pendientes/VentasPendientes';
+//Productos
+import IngresoProducto from '../src/Componentes/Productos/IngresarProducto';
+import BuscarProducto from '../src/Componentes/Productos/BuscarProducto';
+import ActualizarProducto from '../src/Componentes/Productos/ActualizarProducto';
+import EliminarProducto from '../src/Componentes/Productos/EliminarProducto';
+import ListadoProductos from '../src/Componentes/Productos/ListadoProductos';
+import StockCritico from '../src/Componentes/Productos/StockCritico'; 
+//Reportes
+import ReporteGral from '../src/Componentes/Reportes/ReporteGral';
+//Ventas
+import HistorialVentas from '../src/Componentes/Ventas/HistorialVentas';
+import VentaClienteEx from '../src/Componentes/Ventas/VentaClienteEx';
+import VentaClienteNu from '../src/Componentes/Ventas/VentaClienteNu';
 
-  return (
-    <div style={{ display: 'flex' }}>
-      { !shouldHideMenu && <Menu /> }
-      <div style={{ flex: 1 }}>
-        {children}
-      </div>
-    </div>
-  );
-}
 
 function App() {
-  return (
-    <Router>
-      <LayoutWithMenu>
-        <Routes>
-          {/* Supongamos que tu Login está en la ruta "/" o "/login" */}
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* A partir de aquí, el Menu sí se renderiza */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/Bienvenido/inicio" element={<Inicio />} />
-
-          {/* Cliente */}
-          <Route path="/ActualizarCliente" element={<ActualizarCliente />} />
-          <Route path="/BuscarCliente" element={<BuscarCliente />} />
-          <Route path="/EliminarCliente" element={<EliminarCliente />} />
-          <Route path="/ListadoClientes" element={<ListadoClientes />} />
-
-          {/* Empresa */}
-          <Route path="/ActualizarDatos" element={<ActualizarDatos />} />
-          <Route path="/VisualizarDatos" element={<VisualizarDatos />} />
-
-          {/* Pendientes */}
-          <Route path="/VentasPendientes" element={<VentasPendientes />} />
-
-          {/* Productos */}
-          <Route path="/IngresoProducto" element={<IngresoProducto />} />
-          <Route path="/BuscarProducto" element={<BuscarProducto />} />
-          <Route path="/ActualizarProducto" element={<ActualizarProducto />} />
-          <Route path="/EliminarProducto" element={<EliminarProducto />} />
-          <Route path="/ListadoProducto" element={<ListadoProductos />} />
-          <Route path="/StockCritico" element={<StockCritico />} />
-
-          {/* Reportes */}
-          <Route path="/ReporteGral" element={<ReporteGral />} />
-
-          {/* Ventas */}
-          <Route path="/HistorialVentas" element={<HistorialVentas />} />
-          <Route path="/VentaClienteEx" element={<VentaClienteEx />} />
-          <Route path="/VentaClienteNu" element={<VentaClienteNu />} />
-        </Routes>
-      </LayoutWithMenu>
-    </Router>
-  );
+    return (
+            <div style={{ display: 'block' }}>
+                <Menu />
+                <div>
+                    <Routes>
+                        <Route path="" element={<Login />} /> {/* Ruta por defecto para que comience en home*/}
+                        <Route path="/home" element={<Home />} />
+                        {/*Cliente*/}
+                        <Route path="/ActualizarCliente" element={<ActualizarCliente />} />
+                        <Route path="/BuscarCliente" element={<BuscarCliente />} />
+                        <Route path="/EliminarCliente" element={<EliminarCliente />} />
+                        <Route path="/ListadoClientes" element={<ListadoClientes />} />
+                        {/*Empresa*/}
+                        <Route path="/ActualizarDatos" element={<ActualizarDatos />} />
+                        <Route path="/VisualizarDatos" element={<VisualizarDatos />} />
+                        {/*Pendientes*/}
+                        <Route path="/VentasPendientes" element={<VentasPendientes />} />
+                        {/*Productos*/}
+                        <Route path="/IngresoProducto" element={<IngresoProducto />} />
+                        <Route path="/BuscarProducto" element={<BuscarProducto />} />
+                        <Route path="/ActualizarProducto" element={<ActualizarProducto />} />
+                        <Route path="/EliminarProducto" element={<EliminarProducto />} />
+                        <Route path="/ListadoProducto" element={<ListadoProductos />} />
+                        <Route path="/StockCritico" element={<StockCritico />} />
+                        {/*Reporte*/}
+                        <Route path="/ReporteGral" element={<ReporteGral />} />
+                        {/*Ventas*/}
+                        <Route path="/HistorialVentas" element={<HistorialVentas />} />
+                        <Route path="/VentaClienteEx" element={<VentaClienteEx />} />
+                        <Route path="/VentaClienteNu" element={<VentaClienteNu />} />
+                    </Routes>
+                </div>
+            </div>
+    );
 }
 
 export default App;
