b5c904b87029af98099ad304d8ad9c1afc84e900
CdeCevin <kolivaresfuentes@gmail.com>
2025-05-17 20:51:12 -0400

    Agregar Usuario

---
+++ my-react-app/client/src/Componentes/Usuarios/AgregarUsuarios.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from 'react';
+import '../../Estilos/style_menu.css';
+import '../../Estilos/estilo.css';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Reemplaza '#root' con tu selector de raíz
+
+function IngresoUsuario() {
+    const [nombre, setNombre] = useState('');
+    const [telefono, settelefono] = useState('');
+    const [rut, setRut] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [password, setpassword] = useState('');
+    const [modalIsOpen, setModalIsOpen] = useState(false); // Estado para abrir/cerrar el modal
+    const [modalMessage, setModalMessage] = useState(''); // Mensaje para el modal
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        
+        // Agregar los datos del formulario a FormData
+        formData.append('input-nombre', nombre);
+        formData.append('input-rut', rut);
+        formData.append('input-tel', telefono);
+        formData.append('input-tipo', tipo);
+        formData.append('input-password', password);
+        console.log('Datos del formulario:', {
+            telefono, stock, precio, password // Muestra solo los campos numéricos
+        });
+
+        try {
+            // Enviar los datos al backend
+            const response = await fetch('http://localhost:3001/api/ingresar_Usuarios/insertar', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setModalMessage(data.message); // Mostrar mensaje de éxito
+                // Opcional: Reiniciar el formulario
+                resetForm();
+            } else {
+                const errorData = await response.json();
+                setModalMessage(errorData.message); // Mostrar mensaje de error
+            }
+        } catch (error) {
+            console.error('Error al enviar el formulario:', error);
+            setModalMessage('Error al enviar el formulario.'); // Mensaje de error genérico
+        } finally {
+            setModalIsOpen(true); // Abrir el modal después de intentar enviar el formulario
+        }
+    };
+
+    const resetForm = () => {
+        setNombre('');
+        setRut(''); 
+        settelefono('');
+        setStock('');
+        setPrecio('');
+        setColor('');
+        setTipo('');
+        setpassword('');
+        setImagen(null);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    useEffect(() => {
+        document.title = 'Ingresar Usuario';
+    }, []);
+
+    return (
+        <div style={{ marginLeft: '12%' }}>
+            <div className="main-block">
+                <form onSubmit={handleSubmit} encType="multipart/form-data">
+                    <h1>Añadir Usuario</h1>
+                    <fieldset>
+                        <legend>
+                            <h3>Detalles del Usuario</h3>
+                        </legend>
+                        <div className="account-details">
+                            <div>
+                                <label>Nombre*</label>
+                                <input 
+                                    type="text" 
+                                    name="input-nombre" 
+                                    maxLength="50" 
+                                    required 
+                                    value={nombre} 
+                                    onChange={(e) => setNombre(e.target.value)} 
+                                />
+                            </div>
+                            <div>
+                                <label>RUT*</label>
+                                <input 
+                                    type="text" 
+                                    name="input-rut" 
+                                    maxLength="50" 
+                                    required 
+                                    value={rut} 
+                                    onChange={(e) => setNombre(e.target.value)} 
+                                />
+                            </div>
+                            <div>
+                                <label>Telefono*</label>
+                                <input 
+                                    type="text" 
+                                    name="input-tel" 
+                                    pattern="[0-9]+" 
+                                    maxLength="4" 
+                                    required 
+                                    value={telefono} 
+                                    onChange={(e) => settelefono(e.target.value)} 
+                                />
+                            </div>
+                            <div>
+                                <label>Tipo de Usuario</label>
+                                <select 
+                                    name="input-tipo"
+                                    required
+                                    value={tipo}
+                                    onChange={(e) => setTipo(e.target.value)}
+                                >
+                                    <option value="">Seleccionar</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Vendedor">Vendedor</option>
+                                </select>
+                            </div>
+                            <div> 
+                                <label>Contraseña*</label>
+                                <input 
+                                    type="text" 
+                                    name="input-password" 
+                                    maxLength="9" 
+                                    required 
+                                    value={password} 
+                                    onChange={(e) => setpassword(e.target.value)} 
+                                />
+                            </div>
+                        </div>
+                    </fieldset>
+                    <button type="submit">Añadir Usuario</button>
+                </form>
+            </div>
+
+            {/* Modal para mostrar mensajes */}
+            <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Mensaje" className={"custom-modal"}>
+                <h2>Mensaje</h2>
+                <p>{modalMessage}</p>
+                <button onClick={closeModal}>Cerrar</button>
+            </Modal>
+        </div>
+    );
+}
+
+export default IngresoProducto;
