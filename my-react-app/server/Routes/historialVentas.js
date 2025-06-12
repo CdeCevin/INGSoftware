052089f1045c3c916166e2f43d86da55a87bfa25
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-12 18:16:45 -0400

    ahora si?

--- my-react-app/server/Routes/historialVentas.js
+++ my-react-app/server/Routes/historialVentas.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { obtenerReporteVentas } = require('../Controllers/historialVentasController');
+const { obtenerReporteVenta, obtenerBoletaPorCodigo  } = require('../Controllers/historialVentasController');
 const router = express.Router();
 
 router.get('/historialVentas', obtenerReporteVentas);
