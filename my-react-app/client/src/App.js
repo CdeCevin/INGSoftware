f9bc5d523d825cd81ffa24e18575d532efe32445
Domi <92610173+dt-24@users.noreply.github.com>
2025-06-13 18:41:27 -0400

    CAMBIO IMPORTANTE

--- my-react-app/client/src/App.js
+++ my-react-app/client/src/App.js
@@ -6,100 +6,88 @@ import Menu from './Componentes/Bienvenida/menu';
 import Home from './Componentes/Bienvenida/Home';
 import Inicio from './Componentes/Bienvenida/inicio';
 
-// Clientes
-import ActualizarCliente from './Componentes/Clientes/ActualizarCliente';
-import BuscarCliente from './Componentes/Clientes/BuscarCliente';
-import EliminarCliente from './Componentes/Clientes/EliminarCliente';
-import ListadoClientes from './Componentes/Clientes/ListadoClientes';
-// Empresa
-import ActualizarDatos from './Componentes/Empresa/ActualizarDatos';
-import VisualizarDatos from './Componentes/Empresa/VisualizarDatos';
-// Pendientes
-import VentasPendientes from './Componentes/Pendientes/VentasPendientes';
-// Productos
-import IngresoProducto from './Componentes/Productos/IngresarProducto';
-import BuscarProducto from './Componentes/Productos/BuscarProducto';
-import ActualizarProducto from './Componentes/Productos/ActualizarProducto';
-import EliminarProducto from './Componentes/Productos/EliminarProducto';
-import ListadoProductos from './Componentes/Productos/ListadoProductos';
-import StockCritico from './Componentes/Productos/StockCritico';
-// Reportes
-import ReporteGral from './Componentes/Reportes/ReporteGral';
-// Ventas
-import HistorialVentas from './Componentes/Ventas/HistorialVentas';
-import VentaClienteEx from './Componentes/Ventas/VentaClienteEx';
-import VentaClienteNu from './Componentes/Ventas/VentaClienteNu';
+// ... (all your other imports) ...
 
-// Usuarios 
-import AgregarUsuarios from './Componentes/Usuarios/AgregarUsuarios';
-import EliminarUsuario from './Componentes/Usuarios/EliminarUsuario';
-import ActualizarUsuario from './Componentes/Usuarios/ActualizarUsuario';
-import ListadoUsuarios from './Componentes/Usuarios/ListadoUsuarios';
-
-function LayoutWithMenu({ children }) {
-  const { pathname } = useLocation();
-  const hideMenuOn = ['/', '/login'];  // rutas sin menú
-  const shouldHideMenu = hideMenuOn.includes(pathname);
-
+// A component for pages that *do* have the sidebar
+function StandardLayout({ children }) {
   return (
     <div style={{ display: 'flex', minHeight: '100vh', width: '100vw' }}>
-      {!shouldHideMenu && <Menu />}
-      <div className="content">
+      <Menu />
+      <div className="content" style={{ flexGrow: 1 }}> {/* Content takes remaining space */}
         {children}
       </div>
     </div>
   );
 }
 
-function App() {
+// A component for the login page (no sidebar, centered)
+function LoginLayout({ children }) {
   return (
-    <LayoutWithMenu>
-      <Routes>
-        {/* Rutas sin menú */}
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+    <div style={{
+        display: 'flex',
+        minHeight: '100vh',
+        width: '100vw',
+        justifyContent: 'center', // Center content horizontally
+        alignItems: 'center',     // Center content vertically
+        backgroundColor: '#f5f5f5' // Example background for login page
+    }}>
+      {children}
+    </div>
+  );
+}
 
-        {/* Rutas con menú */}
-        <Route path="/home" element={<Home />} />
-        <Route path="/Bienvenido/inicio" element={<Inicio />} />
+function App() {
+  const location = useLocation(); // Keep useLocation here if you prefer to use it to decide which layout to render
 
-        {/* Clientes */}
-        <Route path="/ActualizarCliente" element={<ActualizarCliente />} />
-        <Route path="/BuscarCliente" element={<BuscarCliente />} />
-        <Route path="/EliminarCliente" element={<EliminarCliente />} />
-        <Route path="/ListadoClientes" element={<ListadoClientes />} />
+  // Determine if the current path is a "no sidebar" path
+  const hideMenuOn = ['/', '/login'];
+  const shouldHideMenu = hideMenuOn.includes(location.pathname);
 
-        {/* Empresa */}
-        <Route path="/ActualizarDatos" element={<ActualizarDatos />} />
-        <Route path="/VisualizarDatos" element={<VisualizarDatos />} />
+  return (
+    <Routes>
+      {/* Route for Login page (uses LoginLayout) */}
+      <Route path="/" element={<LoginLayout><Login /></LoginLayout>} />
+      <Route path="/login" element={<LoginLayout><Login /></LoginLayout>} />
 
-        {/* Pendientes */}
-        <Route path="/VentasPendientes" element={<VentasPendientes />} />
+      {/* Routes for pages with the menu (uses StandardLayout) */}
+      <Route path="/home" element={<StandardLayout><Home /></StandardLayout>} />
+      <Route path="/Bienvenido/inicio" element={<StandardLayout><Inicio /></StandardLayout>} />
 
-        {/* Productos */}
-        <Route path="/IngresoProducto" element={<IngresoProducto />} />
-        <Route path="/BuscarProducto" element={<BuscarProducto />} />
-        <Route path="/ActualizarProducto" element={<ActualizarProducto />} />
-        <Route path="/EliminarProducto" element={<EliminarProducto />} />
-        <Route path="/ListadoProducto" element={<ListadoProductos />} />
-        <Route path="/StockCritico" element={<StockCritico />} />
+      {/* Clientes */}
+      <Route path="/ActualizarCliente" element={<StandardLayout><ActualizarCliente /></StandardLayout>} />
+      <Route path="/BuscarCliente" element={<StandardLayout><BuscarCliente /></StandardLayout>} />
+      <Route path="/EliminarCliente" element={<StandardLayout><EliminarCliente /></StandardLayout>} />
+      <Route path="/ListadoClientes" element={<StandardLayout><ListadoClientes /></StandardLayout>} />
 
-        {/* Reportes */}
-        <Route path="/ReporteGral" element={<ReporteGral />} />
+      {/* Empresa */}
+      <Route path="/ActualizarDatos" element={<StandardLayout><ActualizarDatos /></StandardLayout>} />
+      <Route path="/VisualizarDatos" element={<StandardLayout><VisualizarDatos /></StandardLayout>} />
 
-        {/* Ventas */}
-        <Route path="/HistorialVentas" element={<HistorialVentas />} />
-        <Route path="/VentaClienteEx" element={<VentaClienteEx />} />
-        <Route path="/VentaClienteNu" element={<VentaClienteNu />} />
+      {/* Pendientes */}
+      <Route path="/VentasPendientes" element={<StandardLayout><VentasPendientes /></StandardLayout>} />
 
-        {/* Usuarios */}
-        <Route path="/AgregarUsuarios" element={<AgregarUsuarios />} />
-        <Route path="/EliminarUsuario" element={<EliminarUsuario />} />
-        <Route path="/ActualizarUsuario" element={<ActualizarUsuario />} />
-        <Route path="/ListadoUsuarios" element={<ListadoUsuarios />} />
+      {/* Productos */}
+      <Route path="/IngresoProducto" element={<StandardLayout><IngresoProducto /></StandardLayout>} />
+      <Route path="/BuscarProducto" element={<StandardLayout><BuscarProducto /></StandardLayout>} />
+      <Route path="/ActualizarProducto" element={<StandardLayout><ActualizarProducto /></StandardLayout>} />
+      <Route path="/EliminarProducto" element={<StandardLayout><EliminarProducto /></StandardLayout>} />
+      <Route path="/ListadoProducto" element={<StandardLayout><ListadoProductos /></StandardLayout>} />
+      <Route path="/StockCritico" element={<StandardLayout><StockCritico /></StandardLayout>} />
 
-      </Routes>
-    </LayoutWithMenu>
+      {/* Reportes */}
+      <Route path="/ReporteGral" element={<StandardLayout><ReporteGral /></StandardLayout>} />
+
+      {/* Ventas */}
+      <Route path="/HistorialVentas" element={<StandardLayout><HistorialVentas /></StandardLayout>} />
+      <Route path="/VentaClienteEx" element={<StandardLayout><VentaClienteEx /></StandardLayout>} />
+      <Route path="/VentaClienteNu" element={<StandardLayout><VentaClienteNu /></StandardLayout>} />
+
+      {/* Usuarios */}
+      <Route path="/AgregarUsuarios" element={<StandardLayout><AgregarUsuarios /></StandardLayout>} />
+      <Route path="/EliminarUsuario" element={<StandardLayout><EliminarUsuario /></StandardLayout>} />
+      <Route path="/ActualizarUsuario" element={<StandardLayout><ActualizarUsuario /></StandardLayout>} />
+      <Route path="/ListadoUsuarios" element={<StandardLayout><ListadoUsuarios /></StandardLayout>} />
+    </Routes>
   );
 }
 
