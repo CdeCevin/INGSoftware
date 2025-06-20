1701f6895f702fd913b3e23e351fa17d723a1734
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-20 17:55:04 -0400

    probando no poder volver a inicio

--- my-react-app/client/src/Componentes/Bienvenida/inicio.js
+++ my-react-app/client/src/Componentes/Bienvenida/inicio.js
@@ -7,7 +7,7 @@ function Inicio() {
 
 
     const navigate = useNavigate(); 
-
+    const [isLoading, setIsLoading] = useState(true); // Nuevo estado para controlar la carga/verificación
     useEffect(() => {
         const userRole = localStorage.getItem('userRole'); 
         const allowedRoles = ['Administrador', 'Vendedor']; 
@@ -15,6 +15,9 @@ function Inicio() {
       
         if (!localStorage.getItem('token') || !userRole || !allowedRoles.includes(userRole)) {
             navigate('/login'); // Redirige a la página de login si no cumple los requisitos
+        } else {
+            // Si todo está bien, indica que la verificación ha terminado
+            setIsLoading(false); 
         }
     }, [navigate]); 
     useEffect(() => {
@@ -22,19 +25,20 @@ function Inicio() {
     }, []);
 
     
-
-    return (
-        <div>
-            <div class="main-block">
-                <div class="w3-container">
-                <h1>¡Bienvenido!</h1>
-                <h3>Bienvenido a la aplicación de registro web de Outlet A Tu Hogar.</h3>
-                <p>En el menú de su izquierda podrá encontrar las acciones disponibles para el manejo del negocio.
-                </p>
+    if (isLoading) {
+        return (
+            <div>
+                <div class="main-block">
+                    <div class="w3-container">
+                    <h1>¡Bienvenido!</h1>
+                    <h3>Bienvenido a la aplicación de registro web de Outlet A Tu Hogar.</h3>
+                    <p>En el menú de su izquierda podrá encontrar las acciones disponibles para el manejo del negocio.
+                    </p>
+                    </div>
                 </div>
-            </div>
-            </div>
-    );
+                </div>
+        );
+    }
 }
 
 export default Inicio;
