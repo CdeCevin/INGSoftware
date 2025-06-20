ac91bcc8823ed964b061f29c850018a08d5dca0b
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-20 17:32:51 -0400

    xD

--- my-react-app/client/src/Componentes/Bienvenida/menu.js
+++ my-react-app/client/src/Componentes/Bienvenida/menu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom'; // Importa useNavigate
 import '../../Estilos/style_menu.css';
 import '../../Estilos/estilo.css';
