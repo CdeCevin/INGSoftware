707fcbf415415a5b56b1fd1f09037757d3c95ced
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-20 19:06:48 -0400

    trampita

--- my-react-app/client/src/Componentes/Bienvenida/menu.js
+++ my-react-app/client/src/Componentes/Bienvenida/menu.js
@@ -14,7 +14,7 @@ export default function Menu() {
     localStorage.removeItem('token');
     localStorage.removeItem('userRole');
     localStorage.removeItem('userRut'); 
-    document.title = 'Outlet A Tu Hogar';
+    document.title = 'Iniciar Sesión';
     navigate('/login', { replace: true });
   };
 
