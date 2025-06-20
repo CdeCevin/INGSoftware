f9d1c5a2651e227853da9b70168914db53a03f24
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-19 23:26:29 -0400

    turuururun

--- my-react-app/server/server.js
+++ my-react-app/server/server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const projectRootPath = path.resolve(__dirname, '..');
 
@@ -55,8 +53,9 @@ const loginRoutes = require('./Routes/login');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 // Configurar Express para servir archivos estáticos
 app.use('/images/Outlet', express.static(path.join(__dirname, 'public', 'images', 'outlet')));
