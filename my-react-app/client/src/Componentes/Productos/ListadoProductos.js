6aa01ef53e510ac459c32471130a8eee228cf464
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-20 05:14:05 -0400

    desastre de galan

--- my-react-app/client/src/Componentes/Productos/ListadoProductos.js
+++ my-react-app/client/src/Componentes/Productos/ListadoProductos.js
@@ -87,6 +87,7 @@ const ListadoProductos = () => {
     const mostrarImagen = (producto) => {
         // Se cambió para forzar la extensión a .jpg, ya que el backend la guarda así.
         const imageUrl = `/images/Outlet/${producto.Codigo_Producto}.jpg`; 
+        console.log('En front, URL de la imagen:', imageUrl);
         setSelectedImage(imageUrl);
         setModalIsOpen(true);
     };
