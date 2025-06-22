88ecac2694a79f6737ca5bc78d998120d7acab3a
CdeCevin <106358108+CdeCevin@users.noreply.github.com>
2025-06-22 02:07:20 -0400

    soy aweonao

--- my-react-app/server/Routes/login/login.js
+++ my-react-app/server/Routes/login/login.js
@@ -1,6 +1,6 @@
 // routes/auth.js
 const express = require('express');
 const router  = express.Router();
-const { login } = require('../../Controllers/login/Controller');
+const { login } = require('../../Controllers/login/loginController');
 router.post('/', express.json(), login);
 module.exports = router;
